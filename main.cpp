e1f66eb5e77d6f30ba4e25c9906ed7c0e6542904
Diode-exe <pydev@diodescaer.com>
2026-06-19 23:40:06 -0500

    much better way of handling keys
    multiple keys work now

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ int left_score = 0;
 
 bool past_start_screen = false;
 
+bool keys[256] = {false};
+
 void draw_pixel(int x, int y, unsigned char color) {
     if (x >= 0 && x < 320 && y >= 0 && y < 200) {
         vga_memory[y * 320 + x] = color;
@@ -69,6 +71,16 @@ unsigned char read_keyboard_port() {
     return result;
 }
 
+
+void update_keys() {
+    unsigned char sc = read_keyboard_port();
+    if (sc & 0x80) {
+        keys[sc & 0x7F] = false; // break code (key released)
+    } else {
+        keys[sc] = true;         // make code (key pressed)
+    }
+}
+
 void kernel_main() {
     // Clear screen
     for (int i = 0; i < 320 * 200; i++) {
@@ -93,30 +105,31 @@ void kernel_main() {
         // Clear previous ball position
         draw_pixel(ball_x, ball_y, 0x00);
 
-        unsigned char key = read_keyboard_port();
+        // unsigned char key = read_keyboard_port();
+        update_keys();
 
         // Right Paddle Up (Up Arrow)
-        if (key == 0x48 && right_paddle_y > 0) {
+        if (keys[0x48] && right_paddle_y > 0) {
             for(int i = 0; i < 4; i++) draw_pixel(300, right_paddle_y + 16 + i, 0x00);
             right_paddle_y -= 4;
             right_paddle_direction = -1;
         }
         // Right Paddle Down (Down Arrow)
-        if (key == 0x50 && right_paddle_y < 180) {
+        if (keys[0x50] && right_paddle_y < 180) {
             for(int i = 0; i < 4; i++) draw_pixel(300, right_paddle_y + i, 0x00);
             right_paddle_y += 4;
             right_paddle_direction = 1;
         }
 
         // Left Paddle Up (W Key)
-        if (key == 0x11 && left_paddle_y > 0) {
+        if (keys[0x11] && left_paddle_y > 0) {
             for(int i = 0; i < 4; i++) draw_pixel(20, left_paddle_y + 16 + i, 0x00);
             left_paddle_y -= 4;
             left_paddle_direction = -1;
         }
 
         // Left Paddle Down (S Key)
-        if (key == 0x1F && left_paddle_y < 180) {
+        if (keys[0x1F] && left_paddle_y < 180) {
             for(int i = 0; i < 4; i++) draw_pixel(20, left_paddle_y + i, 0x00);
             left_paddle_y += 4;
             left_paddle_direction = 1;
